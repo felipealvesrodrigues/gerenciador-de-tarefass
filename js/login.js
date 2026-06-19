154e8a2eb243b579efb0d1e21f301268adaf1910
Felipe Alves Rodigues <alvesrodriguesfelipe8@gmail.com>
2026-06-19 16:32:35 -0300

    tela de login pronta + GSAP

---
+++ js/login.js
@@ -0,0 +1,59 @@
+const container = document.getElementById('container')
+const registerBtn = document.getElementById('register')
+const loginBtn = document.getElementById('login') 
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add('active')
+})
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove('active')
+})
+
+
+/* GSAP */
+const tl = gsap.timeline({})
+
+/* fall towards center */
+tl.fromTo(
+    '.container',
+    {
+        y: -800,
+        scaleX: .2,
+        scaleY: .5,
+        opacity: 0
+    },
+    {
+        y: 0,
+        scaleX: .2,
+        scaleY: .5,
+        opacity: 1,
+        duration: 1,
+        ease: 'power3.out'
+    }, '-=0.3'
+)
+
+/* Expand verticaly */
+tl.to (
+    '.container',
+    {
+        scaleY: 1,
+        duration: .6,
+        ease: 'power3.out'
+    }
+)
+
+/* Expand horizontally */
+tl.to(
+    '.container',
+    {
+        scaleX: 1,
+        duration: .7,
+        ease: 'power3.out'
+    },'-=0.2'
+)
+
+/* animate form */
+gsap.defaults({opacity: 0, y: -60, ease: 'power2.out', duration: 1.2})
+gsap.from('.login_title', {delay:1.5})
+gsap.from('.login_form > *:not(.login_title)', {delay: 1.7, stagger: .2})
